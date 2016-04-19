7d8e9c7705c4bc44cf4134beede3c495249241e7
Jacob McKim <mckim.jacob@gmail.com>
2016-04-18 21:35:07 -0400

    Create parkingSpace.hpp

---
+++ parkingSpace.hpp
@@ -0,0 +1,15 @@
+//
+//  parkingSpace.hpp
+//  Open-CVTest
+//
+//  Created by McKim™ on 4/17/16.
+//  Copyright © 2016 GVSU. All rights reserved.
+//
+
+#ifndef parkingSpace_hpp
+#define parkingSpace_hpp
+
+#include <stdio.h>
+
+#endif /* parkingSpace_hpp */
+
